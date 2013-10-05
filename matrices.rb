3c74137071a9156cf00ca47a7dea2ba24e668d3f
Edu <edu@ubuntu.(none)>
2013-10-05 19:00:31 +0200

    Operaciones de matrices: Hecho

---
+++ matrices.rb
@@ -0,0 +1,10 @@
+matriz_A = Matrix.build(dimension){rand(max)}
+matriz_B = Matrix.build(dimension){rand(max)}
+
+
+puts "\nEl resultado de la suma es:"
+puts matriz_A + matriz_B
+puts "\nEl resultado de la multiplicación es:"
+puts matriz_A * matriz_B
+
+
